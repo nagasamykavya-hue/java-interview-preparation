5e32aabf5d8db0187f04ecb6fe036c0e26851b12
nagasamykavya-hue <nagasamykavya@gmail.com>
2026-08-05 11:29:04 +0530

    Day 1: Added DataTypes program

---
+++ Day01-Java-Basics/DataTypes.java
@@ -0,0 +1,18 @@
+public class DataTypes {
+
+    public static void main(String[] args) {
+
+        int age = 22;
+        double salary = 25000.50;
+        char grade = 'A';
+        boolean isPlaced = true;
+        String name = "Kavya";
+
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Salary: " + salary);
+        System.out.println("Grade: " + grade);
+        System.out.println("Placed: " + isPlaced);
+
+    }
+}
