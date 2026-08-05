f7e27be1cc64008ce877fb228260bb5f3e0fb1fd
nagasamykavya-hue <nagasamykavya@gmail.com>
2026-08-05 11:21:49 +0530

    Day 1: Added HelloWorld program

---
+++ Day01-Java-Basics/HelloWorld.java
@@ -0,0 +1,10 @@
+public class HelloWorld {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello, Java!");
+        System.out.println("Welcome to my Java Interview Preparation.");
+
+    }
+
+}
